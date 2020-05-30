a03a5f81990496739dd16e91ce0b6e92e429c9d5
jsbaidwan <jsbaidwan5@gmail.com>
2020-05-29 21:54:21 -0700

    Added comment to the common cart function

--- website/src/app/products/cart/cart.component.ts
+++ website/src/app/products/cart/cart.component.ts
@@ -34,6 +34,7 @@ export class CartComponent implements OnInit {
       "total" : total,
       "tax" : "13",
     };
+    // Subscribed function to update cart counter
     this.data.changeMessage(this.cart_arr.length)
   }
   update_cart(index,type) {
